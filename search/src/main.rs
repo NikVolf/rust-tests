bbde876a6021964937ce18f894cf8b7bc95ce5ec
Nikolay Volf <nikvolf@gmail.com>
2015-07-02 00:14:11 +0300

    another great commit

--- search/src/main.rs
+++ search/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::stdin;
 use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::fmt;
 
 // given: dad went fishing
 // :o1 :node :n1,
@@ -26,71 +27,317 @@ use std::collections::VecDeque;
 //     :dist 1.
 // (c) Denis Dyatlov
 
-fn main() {
-    let mut words = Word::parse("The smallest unit of language which has a particular meaning and can be expressed by itself; the smallest discrete, meaningful unit of language".to_string());
-
-    let line = "meaning";
-    words.sort_by(|a, b| ((*a).predicate.abs() as i64).cmp(&((*b).predicate.abs() as i64)));
-    words.retain(|a| a.subject == line);
-    let w = words.first().unwrap();
-    println!("(s){} (p){} (o){}", w.subject, w.predicate, w.object);
+#[derive(Copy, Clone)]
+enum Predicate {
+    Word,
+    Distance,
+    Node
 }
 
-struct Word {
-    object: String,
-    predicate: f64,
-    subject: String
+#[derive(Copy, Clone)]
+enum LiteralValue {
+    Integer(i64),
+    Float(f64),
+    Text(&'static str)
 }
 
-struct WordValue {
-    predicate: f64,
-    subject: String
+#[derive(Copy, Clone)]
+enum ObjectValue {
+    Id(i64),
+    Literal(LiteralValue)
 }
 
-impl Word {
+#[derive(Copy, Clone)]
+struct Fact {
+    subject: i64,
+    predicate: Predicate,
+    object: ObjectValue
+}
 
-    fn new(object: String, predicate: f64, subject: String) {
-        Word { object: object, predicate: predicate, subject: subject };
+static island_radius: usize = 2;
+static island_size: usize = 5;
+
+static mut counter: i64 = 0;
+
+impl Fact {
+    fn new_object_fact(subject_id: i64, predicate: Predicate, object_id: i64) -> Fact {
+        return Fact { subject: subject_id, predicate: predicate, object: ObjectValue::Id(object_id)};
     }
 
-    fn parse(text: String) -> Vec<Word> {
-        let memory_size = 10;
+    fn new_literal_fact(subject_id: i64, predicate: Predicate, literal: LiteralValue) -> Fact {
+        return Fact { subject: subject_id, predicate: predicate, object: ObjectValue::Literal(literal) };
+    }
 
-        let all_words:Vec<&str> = text.split(" ").collect();
-        let mut result:Vec<Word> = Vec::new();
+    fn new_integer_fact(subject_id: i64, predicate: Predicate, value: i64) -> Fact {
+        return Fact { subject: subject_id, predicate: predicate, object: ObjectValue::Literal(LiteralValue::Integer(value))};
+    }
 
-        for position in 0..(all_words.len()) {
-            let word = all_words.get(position).unwrap();
-            let anchor = match position {
-                0...10 => 0,
-                _ => position - memory_size
+    fn new_float_fact(subject_id: i64, predicate: Predicate, value: f64) -> Fact {
+        return Fact { subject: subject_id, predicate: predicate, object: ObjectValue::Literal(LiteralValue::Float(value))};
+    }
+
+    fn new_text_fact(subject_id: i64, predicate: Predicate, value: &'static str) -> Fact {
+        return Fact { subject: subject_id, predicate: predicate, object: ObjectValue::Literal(LiteralValue::Text(value))};
+    }
+
+    fn get_integer_literal(&self) -> i64 {
+        match self.object {
+                ObjectValue::Literal(ref literal) => match *literal {
+                    LiteralValue::Text(s) => { panic!("literal is of text value"); }
+                    LiteralValue::Integer(i) => { return i }
+                    LiteralValue::Float(f) => { return f as i64 }
+                },
+                ObjectValue::Id(id) => { panic!("literal is the identifier"); }
+            }
+    }
+
+    fn get_float_literal(&self) -> f64 {
+        match self.object {
+            ObjectValue::Literal(ref literal) => match *literal {
+                LiteralValue::Text(s) => { panic!("literal is of text value"); }
+                LiteralValue::Integer(i) => { return i as f64 }
+                LiteralValue::Float(f) => { return f }
+            },
+            ObjectValue::Id(id) => { panic!("literal is the identifier"); }
+        }
+    }
+
+    fn get_text_literal(&self) -> String {
+        match self.object {
+                ObjectValue::Literal(ref literal) => match *literal {
+                    LiteralValue::Text(s) => { return s.to_string(); }
+                    LiteralValue::Integer(i) => {
+                        let integer_representation = i.to_string();
+                        return integer_representation;
+                    }
+                    LiteralValue::Float(f) => {
+                        let float_representation = f.to_string();
+                        return float_representation;
+                    }
+                },
+                ObjectValue::Id(id) => { panic!("literal is the identifier"); }
+            }
+    }
+
+    fn new_id() -> i64  {
+        unsafe {
+            counter = counter + 1;
+            return counter;
+        }
+    }
+}
+
+fn literal_to_string(literal: &LiteralValue) -> String {
+    return match *literal {
+        LiteralValue::Text(ref s) => (*s).to_string(),
+        LiteralValue::Integer(ref i) => (*i).to_string(),
+        LiteralValue::Float(ref f) => (*f).to_string()
+    };
+}
+
+fn object_to_string(object_value: &ObjectValue) -> String {
+    return match *object_value {
+        ObjectValue::Literal(ref literal) => literal_to_string(literal),
+        ObjectValue::Id(ref id) => (*id).to_string()
+    }
+}
+
+impl fmt::Display for Fact {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let predicate = match self.predicate {
+                Predicate::Distance => "dist",
+                Predicate::Node => "node",
+                Predicate::Word => "word"
             };
 
-            if position > 0 || anchor > 0 {
-                let memory_word = all_words.get(anchor).unwrap();
-                result.push(Word {
-                    subject: word.to_string(),
-                    predicate: - ((position - anchor) as f64),
-                    object: memory_word.to_string()
-                });
-                result.push(Word {
-                    subject:  memory_word.to_string(),
-                    predicate: (position - anchor) as f64,
-                    object: word.to_string()
-                });
-            }
+        let subject = self.subject;
+
+        let object = object_to_string(&self.object);
+
+        write!(f, "({}, :{}, {})",
+            subject,
+            predicate,
+            object
+        )
+    }
+}
+
+fn split_sentence(text: &'static str) -> (Vec<&'static str>, Vec<&'static str>, Vec<&'static str>, Vec<&'static str>) {
+    let all_words:Vec<&'static str> = text.split(" ").collect();
+
+    let (initial_words, non_initial_words) = all_words.split_at(island_radius);
+    let (middle_words, tail_words) = non_initial_words.split_at(non_initial_words.len() - island_radius);
+
+    return (all_words.clone(), initial_words.to_vec(), middle_words.to_vec(), tail_words.to_vec());
+}
+
+fn parse(text: &'static str) -> Vec<Fact> {
+    let (all_words, initial_words, middle_words, tail_words) = split_sentence(text);
+    let mut result:Vec<Fact> = Vec::new();
+
+    for (initial_index, initial_word) in initial_words.iter().enumerate() {
+        let start = 0;
+        let finish = initial_index + island_radius;
+
+        let island = Fact::new_id();
+
+        for word_index in start..finish {
+            let distance = initial_index as i64 - word_index as i64;
+            let island_word = Fact::new_id();
+            result.push(Fact::new_object_fact(island, Predicate::Node, island_word));
+            result.push(Fact::new_text_fact(island_word, Predicate::Word, all_words.get(word_index).unwrap()));
+            result.push(Fact::new_integer_fact(island_word, Predicate::Distance, distance));
         }
+    }
 
-        return result;
+    let mut index = 0;
+
+    for word_window in all_words.windows(island_size) {
+        let island = Fact::new_id();
+        for (word_index, word) in word_window.iter().enumerate() {
+            let distance = word_index as i64 - island_radius as i64;
+            let island_word = Fact::new_id();
+            let word_value = all_words.get(index + word_index).unwrap();
+            result.push(Fact::new_object_fact(island, Predicate::Node, island_word));
+            result.push(Fact::new_text_fact(island_word, Predicate::Word, word_value));
+            result.push(Fact::new_integer_fact(island_word, Predicate::Distance, distance as i64));
+        }
+        index = index + 1;
+    }
+
+    return result;
+}
 
 
+fn main() {
+}
+
+const example: &'static str =
+"In linguistics a word is the smallest element that may be uttered in isolation with semantic or\
+pragmatic content (with literal or practical meaning). This contrasts with a morpheme, which is\
+the smallest unit of meaning but will not necessarily stand on its own.";
+
+#[test]
+fn it_parses() {
+    let facts = parse(example);
+    assert_eq!(585, facts.len())
+}
+
+#[test]
+fn it_finds_facts_for_contrasts() {
+    let facts = parse(example);
+    let word = "contrasts".to_string();
+    let word_facts:Vec<&Fact> = facts.iter().filter(
+        |ref f| match f.object {
+            ObjectValue::Literal(ref literal) =>
+                match *literal {
+                    LiteralValue::Text(ref s) => *s == word,
+                    _ => false
+                },
+            _ => false
+        }).collect();
+
+    assert_eq!(island_size, word_facts.len());
+}
+
+fn collect_word_facts (facts: &Vec<Fact>, word: &'static str) -> Vec<Fact> {
+    return facts.iter().filter(
+        |f| match f.object {
+            ObjectValue::Literal(ref literal) =>
+                match *literal {
+                    LiteralValue::Text(s) => s == word,
+                    _ => false
+                },
+            _ => false
+        })
+        .map(|x| *x)
+        .collect();
+}
+
+fn resolve_word_distance(facts: &Vec<Fact>, subject: i64) -> Fact {
+    let facts:Vec<Fact> = facts.iter().filter(
+        |f| match f.predicate {
+            Predicate::Distance => f.subject == subject,
+            _ => false
+        })
+        .map(|f|*f)
+        .collect();
+
+    return *(facts.first().unwrap());
+}
+
+#[test]
+fn it_finds_positive_facts_for_contrasts() {
+    let facts = parse(example);
+    let word_facts = collect_word_facts(&facts, "contrasts");
+
+    let positive_facts:Vec<Fact> =
+        word_facts.iter()
+            .filter(|x| resolve_word_distance(&facts, x.subject).get_float_literal() > 0.0)
+            .map(|x| *x)
+            .collect();
+
+    for fact in positive_facts.iter() {
+        println!("[:{}, {}] :{} 'contrasts'",
+            fact.subject,
+            resolve_word_distance(&facts, fact.subject),
+            match fact.predicate {
+                Predicate::Distance => "dist",
+                Predicate::Node => "node",
+                Predicate::Word => "word"
+            }
+        );
     }
 }
 
 #[test]
-fn it_parses() {
-    let words = Word::parse("The smallest unit of language which has a particular meaning and can be expressed by itself; the smallest discrete, meaningful unit of language".to_string());
-    for w in words.iter() {
-        println!("(s){} (p){} (o){}", w.subject, w.predicate, w.object);
-    }
+fn it_finds_next_word_for_contrasts() {
+    let facts = parse(example);
+    let word_facts: Vec<Fact> = collect_word_facts(&facts, "contrasts");
+
+    let previous_word_facts:Vec<Fact> = word_facts
+        .iter()
+        .filter(|x| (resolve_word_distance(&facts, (*x).subject).get_integer_literal() == -1))
+        .map(|x| *x)
+        .collect();
+
+    let previous_word_fact:Fact = *(previous_word_facts.first().unwrap());
+
+    println!("previous word: {}", previous_word_fact);
+
+//
+//            .collect()
+//            .first();
+
+//    let fact0 =
+//        facts.iter()
+//            .filter(|x:Fact| resolve_word_distance(&facts, fact.subject).get_integer_literal() == 0)
+//            .map(|x| * x);
+//
+
+}
+
+#[test]
+fn it_can_split_to_sentence_epochs() {
+    let (all, p1, p2, p3) = split_sentence("A great day to actually die");
+
+    assert_eq!(["A", "great"].to_vec(), p1);
+    assert_eq!(["actually", "die"].to_vec(), p3);
+}
+
+#[test]
+fn vec_is0based() {
+    assert_eq!(["A"].to_vec().get(0).unwrap(), &"A");
+}
+
+#[test]
+fn vec_lenIsActual() {
+    assert_eq!(["A"].to_vec().len(), 1);
+}
+
+#[test]
+fn can_return_object_value_of_float_from_fact_with_literal() {
+    let f = Fact::new_float_fact(1231, Predicate::Distance, 10.0);
+    let object_value = f.get_float_literal();
+    assert_eq!(10.0, object_value);
 }
